44add29c64bf6d40a02793ca44ef0e9fd5bf1200
vasperia-cyber <elmghari90@hotmail.com>
2026-04-09 01:02:25 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Header />
      {/* 2. West l-JSX, kat-7ti ghir l-tag dyal l-component */}
-      <NativeAd />
+    
       
       <main className="flex-1">
         {children}
